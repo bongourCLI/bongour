55a0b0ac1d6647fba2e92f104dcaa341b13e707e
Vivek <vivekpuri420@gmail.com>
2016-02-12 15:30:44 +0530

    edition in alert script

--- Alert.sh
+++ Alert.sh
@@ -1,6 +1,7 @@
-string=`sudo service mysqld status`
-if [[ $string == *"stopped"* ]]
-then
-        mail -s "Alert!!! MySQL stopped!!!" devops@clicklabs.in <<< "Service MySQL is stopped. Please check."
-fi
+string=`sudo service mysqld status`;
+recipients="devops@clicklabs.in, vikas.kumar@clicklabs.in, kunal.sethiya@click-labs.com, sanjay@click-labs.com"
 
+if [[ $string != *"running"* ]]
+then
+	echo -e "Here is the status of MySQL service on Tookan website server \n<< $string >>\nPlease check." | mail -s "Alert!!! MySQL service stopped on tookan website (54.173.213.236)!!!" $recipients
+fi
