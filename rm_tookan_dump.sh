98cce4b6407f6b928fac82418e5b6047938be3ff
Vivek Puri <vivekpuri1995@gmail.com>
2016-02-25 12:16:46 +0000

    rm_tookan_dump.sh edited online with Bitbucket

--- rm_tookan_dump.sh
+++ rm_tookan_dump.sh
@@ -4,7 +4,7 @@ for i in `aws s3 ls s3://tookandumps | awk '{print $4}'`
 do 
 	dt=`echo $i | rev | cut -c 9- |  cut -c -16 | rev`
 	diff=`echo $(( ( $(date -ud "$cur_dt" +'%s') - $(date -ud "$dt" +'%s') )/60/60 ))`
-	if [[ $diff >= 24 ]]; then
+	if [[ "$diff" -ge 24 ]]; then
 		echo "Removing file $i"	
 		aws s3 rm s3://tookandums/$i
 	fi
