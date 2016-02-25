35cb1b2ce67677d76ba04bf0132541024b32d3e9
Vivek <vivekpuri420@gmail.com>
2016-02-25 17:25:40 +0530

    dump_removal

--- rm_tookan_dump.sh
+++ rm_tookan_dump.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
-dt=`date +"%H-%M-%d-%m-%Y"`
-timing=""
-for i in `aws s3 ls s3://tookandumps | awk '{print $4}'| sort`
+cur_dt=`date +"%Y-%m-%d %H:%M"`
+for i in `aws s3 ls s3://tookandumps | awk '{print $4}'`
 do 
-	cur_time=`echo $i | rev | cut -c 20- | rev`
-	if [[ "$timing" == "$cur_time" ]]; then
+	dt=`echo $i | rev | cut -c 9- |  cut -c -16 | rev`
+	diff=`echo $(( ( $(date -ud "$cur_dt" +'%s') - $(date -ud "$dt" +'%s') )/60/60 ))`
+	if [[ $diff >= 24 ]]; then
 		echo "Removing file $i"	
 		aws s3 rm s3://tookandums/$i
-	else
-		timing=$cur_time
 	fi
 done
+
+
